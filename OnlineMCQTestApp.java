1e40cac5c04e5534b03b1ac7f16963d27771adaa
Sohail Saifi <sohailsaifi65@gmail.com>
2020-11-22 00:49:46 +0530

    Variables added

---
+++ OnlineMCQTestApp.java
@@ -0,0 +1,21 @@
+package Projects;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+
+class OnlineMCQTestApp extends JFrame implements ActionListener {
+	
+	JLabel label;
+	JRadioButton radioButton[] = new JRadioButton[5];
+	JButton btnNext, btnBookmark;
+	ButtonGroup bg;
+	int count = 0, current = 0, x = 1, y = 1, now = 0;
+	int m[] = new int[10];
+}
