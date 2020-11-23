f46b1c60d9b6c1eeadf7dadd1c36fc2fd29c97c5
Sohail Saifi <sohailsaifi65@gmail.com>
2020-11-23 15:36:59 +0530

    Actions Added

--- OnlineMCQTestApp.java
+++ OnlineMCQTestApp.java
@@ -17,5 +17,66 @@ class OnlineMCQTestApp extends JFrame implements ActionListener {
 	JButton btnNext, btnBookmark;
 	ButtonGroup bg;
 	int count = 0, current = 0, x = 1, y = 1, now = 0;
-	int m[] = new int[10];
+    int m[] = new int[10];
+    
+    // create jFrame with radioButton and JButton
+	OnlineMCQTestApp(String s) {
+		super(s);
+		label = new JLabel();
+		add(label);
+		bg = new ButtonGroup();
+		for (int i = 0; i < 5; i++) {
+			radioButton[i] = new JRadioButton();
+			add(radioButton[i]);
+			bg.add(radioButton[i]);
+		}
+		btnNext = new JButton("Next");
+		btnBookmark = new JButton("Bookmark");
+		btnNext.addActionListener(this);
+		btnBookmark.addActionListener(this);
+		add(btnNext);
+		add(btnBookmark);
+		set();
+		label.setBounds(30, 40, 450, 20);
+		//radioButton[0].setBounds(50, 80, 200, 20);
+		radioButton[0].setBounds(50, 80, 450, 20);
+		radioButton[1].setBounds(50, 110, 200, 20);
+		radioButton[2].setBounds(50, 140, 200, 20);
+		radioButton[3].setBounds(50, 170, 200, 20);
+		btnNext.setBounds(100, 240, 100, 30);
+		btnBookmark.setBounds(270, 240, 100, 30);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(null);
+		setLocation(250, 100);
+		setVisible(true);
+		setSize(600, 350);
+	}
+	// handle all actions based on event
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnNext) {
+			if (check())
+				count = count + 1;
+			current++;
+			set();
+			if (current == 9) {
+				btnNext.setEnabled(false);
+				btnBookmark.setText("Result");
+			}
+		}
+		if (e.getActionCommand().equals("Bookmark")) {
+			JButton bk = new JButton("Bookmark" + x);
+			bk.setBounds(480, 20 + 30 * x, 100, 30);
+			add(bk);
+			bk.addActionListener(this);
+			m[x] = current;
+			x++;
+			current++;
+			set();
+			if (current == 9)
+				btnBookmark.setText("Result");
+			setVisible(false);
+			setVisible(true);
+		}
+	
+	}
 }
